0979f9626941d3a2dae7573be3bd60980e393db8
Yunus Ahmet AKDAL <yunus.akdal@gmail.com>
2025-03-23 21:32:40 +0300

    Add Plots and plot functions

--- Homeworks/homework-1/q1/init.m
+++ Homeworks/homework-1/q1/init.m
@@ -3,23 +3,29 @@ clear; clc;
 
 modelName = "model";
 sample_time = 0.01;
-simTime = 20;
-
+simTime = 30;
+iter = 5
 k0 = 2;
-gamma = 0.5;
 
-out = sim(modelName);
 
-name = "gamma_" + gamma*1000;
+for i = 1:iter
+    gamma = 0.25*2^(i-1)/2;
+    
+    out = sim(modelName);
+    
+    name = "gamma_" + gamma*1000;
+    
+    data.(name).uc = out.uc;
+    data.(name).ym = out.ym;
+    data.(name).theta = out.theta;
+    data.(name).y = out.y;
+    
+    sys_theta = tf([4*gamma,4*gamma],[1 1 2*gamma]); %5.12
+    
+    [temp_data,time]= step(sys_theta);
+    data.(name).model = timeseries(temp_data,time);
+end
 
-data.(name).ym = out.ym;
-data.(name).theta = out.theta;
-data.(name).y = out.y;
-
-sys_theta = tf([4*gamma,4*gamma],[1 1 2*gamma]); %5.12
-
-[temp_data,time]= step(sys_theta);
-data.(name).model = timeseries(temp_data,time);
-
+% plot_data(data.(name));
 
 
--- Homeworks/homework-1/q1/plot_data.m
+++ Homeworks/homework-1/q1/plot_data.m
@@ -1,10 +1,9 @@
-function  plot_graph(data)
-
-    
+function  plot_data(data)
     figure(1);
     hold on;
     plot(data.ym,"DisplayName","Ym");
     plot(data.y,"DisplayName","Y");
+    plot(data.uc,"DisplayName","Uc");
     legend()
     grid;
     figure(2);
