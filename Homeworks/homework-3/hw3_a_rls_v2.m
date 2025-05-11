6d3efed12badc12d7ab9a9d0ecc6bef3a785a8b8
kemalmustafaarik <kemalmustafaarik@gmail.com>
2025-05-11 22:17:26 +0300

    gerekli güncellemeler yapıldı

--- Homeworks/homework-3/hw3_a_rls_v2.m
+++ Homeworks/homework-3/hw3_a_rls_v2.m
@@ -4,8 +4,8 @@ sample_time = 1;
 
 a = -0.8;
 b = 0.5;
-c = 0;
-% c = -0.5;
+% c = 0;
+c = -0.5;
 
 % noise
 noise_mean = 0;
@@ -17,7 +17,7 @@ if c==0
     phi_0 = zeros(2,1);
     P_0 = eye(2,2) * 100;
     Theta_0 = [0;0];
-    sim("hw3_a_RLS_model_c0");
+    % sim("hw3_a_rls_model_c0");
     % sim("hw3_a_RLS_lambda_model_c0");
     % sim("hw3_a_LMS_model_c0");
     % sim("hw3_a_PA_model_c0");
@@ -26,5 +26,7 @@ else
     phi_0 = zeros(3,1);
     P_0 = eye(3,3) * 100;
     Theta_0 = [0;0;0];
-    sim("hw3_a_ELS_model_c05"); 
+    % sim("hw3_a_ELS_model_c05");
+    % sim("hw3_a_rls_model_c5");
+    % sim("hw3_a_ELS_model_extended_c05");
 end
